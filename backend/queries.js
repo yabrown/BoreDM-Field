52eea8286d777a685bc04bbc3d5ab70449950565
louisaaron <laaron@princeton.edu>
2022-10-28 12:30:29 -0400

    add comments, empty testfile

--- backend/queries.js
+++ backend/queries.js
@@ -3,6 +3,9 @@ var conString = "postgres://iuskfbrh:6tDvSAVRHoYJm7TqtLjlXeb2o-mjH6sz@batyr.db.e
 var pg = require('pg')
 var client = new pg.Client(conString);
 
+// creates a persistent connection to the elephantsql db. if persistence is undesirable (and we instead
+// want to open and close connections with each query, we will need to rewrite this)
+// written by: Max and Louis
 client.connect(function(err) {
   if(err) {
     return console.error('could not connect to postgres', err);
@@ -13,19 +16,24 @@ client.connect(function(err) {
     }
     console.log(result.rows[0].theTime);
   });
-
 });
 
+// uses the client connection above to query for a list of projects from elephantsql
+// written by: Max and Louis
 async function get_projects() {
   result = await client.query('SELECT * FROM "public"."projects"');
   return result.rows;
 }
 
+// uses the client connection above to query for the projects with project_id=project_id from elephantsql
+// written by: Max and Louis
 async function get_project(project_id) {
   result = await client.query('SELECT * FROM "public"."projects" WHERE project_id=' + project_id);
   return result.rows;
 }
 
+// exports the functions in queries.js so they can be used in index.js (and potentially elsewhere)
+// written by: Max and Louis
 module.exports = {
   get_projects,
   get_project,
