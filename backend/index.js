e5abd881e2dfa439f3770cb92fa3e19823d00996
Daniel Friedman <dannymf0@gmail.com>
2022-11-02 14:27:53 -0400

    bidirectional connection and logging request on backend

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,25 @@ const cors = require('cors');
 const PORT = 4000;
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 // get request on the root directory, displays a list of projects in json format on the broswer
 // written by: Max and Louis
 app.get('/', async (req, res) => {
   try {
       const results = await db.get_all_project_names();
       res.json(results);
+  } catch (err) {
+      console.log(err);
+  }
+})
+
+app.post('/post', (req, res) => {
+  try {
+      console.log("req.body: ", req.body);
+      res.json(req.body);
+      res.status(200).send();
   } catch (err) {
       console.log(err);
   }
