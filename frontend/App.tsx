5a31bc81cdf06e90d890fbdfe5d988ed8e531693
Daniel Friedman <dannymf0@gmail.com>
2022-11-03 15:41:26 -0400

    restructure app.tsx

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,34 +1,40 @@
-import { stringLiteral } from '@babel/types';
 import { StatusBar } from 'expo-status-bar';
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import { Button, StyleSheet, TextInput, Text, View, SafeAreaView, ScrollView, TouchableHighlight, Alert } from 'react-native';
 
 
+function SubmitProject(props:{text: string}) {
+    const onPress = async () => {
+        try {
+            let fetched = await fetch('http://localhost:4000/post', {
+                method: 'POST', // or 'PUT'
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({project_name: props.text})
+            })
+            let json_text = await fetched.json()
+            console.log(json_text)
+        } catch(error) {
+                console.error('Error:', error);
+            }
+    }
+
+    return (<Button
+        onPress={onPress}
+        title="Add Project"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"/>);
+}
+
 export default function App() {
     const [text, onChangeText] = useState("Enter Project name");
-    const onPress = () => {
-        fetch('localhost:4000/post', {
-            method: 'POST', // or 'PUT'
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(text),
-            })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log('Success:', data);
-            })
-            .catch((error) => {
-                console.error('Error:', error);
-        });
-    }
     return (
         <View style={styles.container}>
             <Header/>
             <Title name="Project"/>
-            <StatusBar style="auto" />
+            <StatusBar style="auto"/>
             <SelectProjectList/>
-            
 
             <SafeAreaView>
                 <TextInput
@@ -36,40 +42,33 @@ export default function App() {
                     value={text}
                 />
             </SafeAreaView>
-            <Button
-            onPress={onPress}
-            title="Add Project"
-            color="#841584"
-            accessibilityLabel="Learn more about this purple button"/>
-        
-
-
+            <SubmitProject text={text}/>
         </View>
     );
 }
 
-const AddProjectButton = () => {
-     return(
-        <Button
-        onPress={onPress}
-        title="Add Project"
-        color="#841584"
-        accessibilityLabel="Learn more about this purple button"/>
-     )
-}
+// const AddProjectButton = () => {
+//      return(
+//         <Button
+//         onPress={onPress}
+//         title="Add Project"
+//         color="#841584"
+//         accessibilityLabel="Learn more about this purple button"/>
+//      )
+// }
 
-const UselessTextInput = () => {
-    const [text, onChangeText] = React.useState("Enter Project name");
+// const UselessTextInput = () => {
+//     const [text, onChangeText] = React.useState("Enter Project name");
   
-    return (
-      <SafeAreaView>
-        <TextInput
-          onChangeText={onChangeText}
-          value={text}
-        />
-      </SafeAreaView>
-    );
-  };
+//     return (
+//       <SafeAreaView>
+//         <TextInput
+//           onChangeText={onChangeText}
+//           value={text}
+//         />
+//       </SafeAreaView>
+//     );
+//   };
 
 const onPress = () => {
   Alert.alert("Button clicked")
@@ -87,13 +86,13 @@ const SelectProjectList = () => {
     const [data, setData] = useState<project[]>([{project_id: -1, project_name: "default"}])
     //const [data, setData] = useState<void>()
 
-    const GetProjects = async () => {
+    const GetProjects: () => void = async () => {
         try{
-            await fetch('http://localhost:4000/').then(res => res.json()).then(data => setData(data))
-            //let data = fetched.json()
-        }catch(error){
+            let fetched = await fetch('http://localhost:4000/');
+            let res_data = await fetched.json()
+            setData(res_data)
+        } catch(error) {
             console.error(error)
-
         }
     }
 
@@ -121,7 +120,7 @@ const Header = () =>{
 }
 
 // Returns text to go above changing view-- Ex: Project, Map, Mariner's Apartment 
-const Title = (props: props) =>{
+const Title = (props: { name:string }) =>{
     return(
         <View style={styles.titleView}>
             <Text style={{fontWeight:'500', fontSize: 20, color: 'black'}}>{props.name}</Text>
@@ -129,13 +128,8 @@ const Title = (props: props) =>{
     )
 }
 
-// Props for SelectProjectButton
-interface props {
-    name: string;
-  }
-
 // Takes in a name, returns a button that takes you to the project page
-const SelectProjectButton = (props: props) => {
+const SelectProjectButton = (props: { name:string }) => {
    return(
         <TouchableHighlight style={styles.touchable} onPress={onPress} activeOpacity={.8} underlayColor={"#00000011"}>
             <View style={styles.button}>
