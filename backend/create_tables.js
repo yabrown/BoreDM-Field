14b395647bc171a687271ec786d49f69620018ab
louisaaron <laaron@princeton.edu>
2022-11-07 02:01:14 -0500

    backend is our bitch

--- backend/create_tables.js
+++ backend/create_tables.js
@@ -5,37 +5,53 @@ var client = new pg.Client(conString);
 const { Sequelize, Model, DataTypes, DATE, FLOAT } = require("sequelize");
 const sequelize = new Sequelize(conString);
 
-const Project = sequelize.define("project", {
-  name: DataTypes.TEXT,
-  location: DataTypes.TEXT,
-  client: DataTypes.TEXT,
-  notes: DataTypes.TEXT,
-});
+module.exports = (sequelize) => {
+  const Project = sequelize.define("Project", {
+    name: DataTypes.TEXT,
+    location: DataTypes.TEXT,
+    client: DataTypes.TEXT,
+    notes: DataTypes.TEXT,
+  });
+  return Project;
+}
 
-const Log = sequelize.define("log", {
-  project_id: DataTypes.UUID,
-  driller: DataTypes.INTEGER,
-  logger: DataTypes.INTEGER,
-  notes: DataTypes.TEXT,
-  location: DataTypes.UUID,
-  date: DataTypes.DATE,
-});
+module.exports = (sequelize) => {
+  const Log = sequelize.define("Log", {
+    project_id: DataTypes.INTEGER,
+    driller: DataTypes.TEXT,
+    logger: DataTypes.TEXT,
+    notes: DataTypes.TEXT,
+    location: DataTypes.INTEGER,
+    // date: DataTypes.DATE,
+  });
+  return Log;
+}
 
-// const Sample = sequelize.define("sample", {
+// const Sample = sequelize.define("Sample", {
 //   log_id: DataTypes.UUID,
 // });
 
-// const Classification = sequelize.define("classification", {
+// const Classification = sequelize.define("Classification", {
 //   project_id: DataTypes.UUID,
 // });
 
-const Coordinates = sequelize.define("coordinates", {
-  location_id: DataTypes.UUID,
-  latitude: DataTypes.FLOAT,
-  longitude: DataTypes.FLOAT,
-});
+module.exports = (sequelize) => {
+  const Coordinate = sequelize.define("Coordinate", {
+    location_id: DataTypes.INTEGER,
+    latitude: DataTypes.FLOAT,
+    longitude: DataTypes.FLOAT,
+  });
+  return Coordinate;
+}
 
 (async () => {
   await sequelize.sync({ force: true });
-  // Code here
+
+  const project_1 = await Project.create({ name: "Kuba", location: "Princeton, NJ", client: "Alicki", notes: "Test Project 1"});
+  const home_1 = await Coordinate.create({ latitude: 10, longitude: 15 });
+  const log_1 = await Log.create({ project_id: project_1.id, name: "Test Log 1", driller: "Danny", logger: "Ari", notes: "Nice", location: home_1.id});
+  
+  const project_2 = await Project.create({ name: "Kuba", location: "Princeton, NJ", client: "Alicki", notes: "Test Project 2"});
+  const home_2 = await Coordinate.create({ latitude: 10, longitude: 15 });
+  const log_2 = await Log.create({ project_id: project_2.id, name: "Test Log 2", driller: "Louis", logger: "Max", notes: "Very nice!", location: home_2.id});
 })();
--- backend/queries.js
+++ backend/queries.js
@@ -3,25 +3,43 @@ var conString = "postgres://iuskfbrh:6tDvSAVRHoYJm7TqtLjlXeb2o-mjH6sz@batyr.db.e
 var pg = require('pg')
 var client = new pg.Client(conString);
 
-// const { Sequelize, DataTypes } = require('sequelize');
-// const sequelize = new Sequelize(conString);
+const { Sequelize, Model, DataTypes, DATE, FLOAT } = require("sequelize");
+const sequelize = new Sequelize(conString);
 
-// const project = sequelize.define('project', {
-//   // Model attributes
-//   project_id: {
-//     type: DataTypes.INTEGER,
-//     primaryKey: true
-//   },
-//   project_name: DataTypes.STRING,
-//   city_state:  DataTypes.STRING,
-//   client_name: DataTypes.STRING,
-//   project_notes: DataTypes.STRING,
-// },{
-//   timestamps: false
-// });
+const Project = sequelize.define("Project", {
+  name: DataTypes.TEXT,
+  location: DataTypes.TEXT,
+  client: DataTypes.TEXT,
+  notes: DataTypes.TEXT,
+});
 
+const Log = sequelize.define("Log", {
+  project_id: DataTypes.INTEGER,
+  driller: DataTypes.TEXT,
+  logger: DataTypes.TEXT,
+  notes: DataTypes.TEXT,
+  location: DataTypes.INTEGER,
+  // date: DataTypes.DATE,
+});
 
-// const projects = await projects.findAll()
+const Coordinate = sequelize.define("Coordinate", {
+  location_id: DataTypes.INTEGER,
+  latitude: DataTypes.FLOAT,
+  longitude: DataTypes.FLOAT,
+});
+
+(async () => {
+  await sequelize.sync();
+
+  const project_1 = await Project.create({ name: "Kuba", location: "Princeton, NJ", client: "Alicki", notes: "Test Project 1"});
+  const home_1 = await Coordinate.create({ latitude: 10, longitude: 15 });
+  const log_1 = await Log.create({ project_id: project_1.id, name: "Test Log 1", driller: "Danny", logger: "Ari", notes: "Nice", location: home_1.id});
+  
+  const project_2 = await Project.create({ name: "Kuba", location: "Princeton, NJ", client: "Alicki", notes: "Test Project 2"});
+  const home_2 = await Coordinate.create({ latitude: 10, longitude: 15 });
+  const log_2 = await Log.create({ project_id: project_2.id, name: "Test Log 2", driller: "Louis", logger: "Max", notes: "Very nice!", location: home_2.id});
+})();
+
 
 // creates a persistent connection to the elephantsql db. if persistence is undesirable (and we instead
 // want to open and close connections with each query, we will need to rewrite this)
@@ -40,22 +58,22 @@ client.connect(function(err) {
 
 // uses the client connection above to query for a list of projects from elephantsql
 // written by: Max and Louis
-async function get_all_project_names(Project) {
+async function get_all_project_names() {
   result = await Project.findAll();
   return result;
 }
 
 // uses the client connection above to query for the projects with project_id=project_id from elephantsql
 // written by: Max and Louis
-// async function get_project(project_id) {
-//   result = await client.query('SELECT * FROM "public"."projects" WHERE project_id=' + project_id);
-//   return result.rows;
-// }
+async function get_project(project_id) {
+  result = await Project.findByPk(project_id);
+  return result;
+}
 
 // exports the functions in queries.js so they can be used in index.js (and potentially elsewhere)
 // written by: Max and Louis
 module.exports = {
   get_all_project_names,
-  // get_project,
+  get_project,
   client,
 }
