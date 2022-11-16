d922aeadd872304c20fe563e35566389a3c10ff2
Ari Brown <yabrown@princeton.edu>
2022-11-16 16:35:52 -0500

    tiny backend change to hopefully fix cors/render issue

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors');
 const PORT = process.env.port || 4000;
 
-app.use(cors())
+app.use(cors({origin: '*'}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
