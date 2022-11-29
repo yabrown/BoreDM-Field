7984f96b2888fd7565e4039972a10ecd1bd19d33
Ari Brown <yabrown@princeton.edu>
2022-11-29 14:19:53 -0500

    minor: cors change

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const jwt = require('jsonwebtoken')
 const { PORT, ACCESS_TOKEN_SECRET } = env;
 const app = express();
 
-app.use(cors({origin: '*'}))
+app.use(cors({origin: '*', "methods": "GET,HEAD,PUT,PATCH,POST,DELETE"}))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
