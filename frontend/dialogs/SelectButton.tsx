ade8011babd90cd5c7c3cf2b45ed3a8c6894ee1c
Max Chan <maxchan@Louiss-MacBook.local>
2022-12-12 02:36:44 -0500

    Updating + Classification so that it actually looks nice

--- frontend/dialogs/SelectButton.tsx
+++ frontend/dialogs/SelectButton.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { View } from "react-native";
 import { Button } from 'react-native-paper';
 
-const SelectButton = ({ current, buttonOption, setFunction, color, highlightedColor="lightgrey" }) => (  
-  <View style={{ minWidth: 140, margin: 4 }}>
+const SelectButton = ({ current, buttonOption, setFunction, color, highlightedColor="lightgrey" }) => (
+  <View style={{ minWidth: 150, maxWidth: 150, margin: 5 }}>
     <Button
-      style={{borderColor: color }}
+      style={{borderColor: "#696969" }}
       buttonColor={current===buttonOption ? highlightedColor : color}
       textColor='black'
       mode='outlined'
